58a9b3b8485fdb20eeff117c56eb3e2bc5a382d8
Martin Jorge Nicolas <nicomartin854@gmail.com>
2022-05-10 21:00:20 -0300

    FIX: bug github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://nicomartin8542.github.io/",
+  base: "https://nicomartin8542.github.io/index.html/",
   plugins: [react()],
 });
